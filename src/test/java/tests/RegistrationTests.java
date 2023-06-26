b3ac1009fbdfb8003ad23cdf38b86ea31df241c2
MikeTestin9 <Michael_benin@hotmail.com>
2023-06-26 13:04:14 +0300

    CW9

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -7,19 +8,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase{
+
     @Test
     public void registrationPositive(){
-
         int i = (int)(System.currentTimeMillis()/1000)%3600;
-        String email = "mb" + i + "@gmail.com", password = "Mb12345$";
-        app.getUser().openLoginForm();       //open login form
-        app.getUser().fillLoginForm(email, password);   //fill login form
-        app.getUser().submitRegistration();     //click on button Registration
-        app.getUser().pause(3000);   //pause for 3 seconds
+
+        User user = new User()
+                .withEmail("mb" + i + "@gmail.com")
+                .withPassword("Mb12345$")
+                ;
+        app.getUser().openLoginForm();                    //open login form
+        app.getUser().fillLoginForm(user);                //fill login form
+        app.getUser().submitRegistration();               //click on button Registration
+        app.getUser().pause(3000);                  //pause for 3 seconds
         Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button")));
-
-
     }
+
 
     @Test
     public void registrationNegativeWrongEmail(){
